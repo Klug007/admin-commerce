c20da3ffc65594365d35cb0bcc0a08d4f6ae1c09
Klug007 <tilla.yosinbek@gmail.com>
2024-07-31 17:26:43 +0500

    false

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => {
     return {
-      isAuthenticated: true,
+      isAuthenticated: false,
       users: [{ email: 'admin', password: 'password' }]
     }
   },
