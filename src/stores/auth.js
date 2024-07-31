b7d9ec5ea2b9139135f8ef4981fe425fabaad103
Klug007 <tilla.yosinbek@gmail.com>
2024-07-31 13:44:37 +0500

    little fix

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,13 +4,13 @@ export const useAuthStore = defineStore('auth', {
   state: () => {
     return {
       isAuthenticated: false,
-      users: []
+      users: [{ email: 'admin', password: 'password' }]
     }
   },
   actions: {
-    async login(email,password) {
-      const user = this.users.find(user => user.email === email && user.password === password)
-      if(user){
+    async login(email, password) {
+      const user = this.users.find((user) => user.email === email && user.password === password)
+      if (user) {
         this.isAuthenticated = true
         return true
       } else {
@@ -21,9 +21,9 @@ export const useAuthStore = defineStore('auth', {
       this.isAuthenticated = false
     },
     async register(email, password) {
-      const userExists = this.users.some(user => user.email === email)
-      if(!userExists) {
-        this.users.push({email, password})
+      const userExists = this.users.some((user) => user.email === email)
+      if (!userExists) {
+        this.users.push({ email, password })
         this.isAuthenticated = true
         return true
       } else {
